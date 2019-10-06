592fd0647b9ba6df09d02aeaf79d3316a1534ca9
zhouquanying <2831309803@qq.com>
2019-10-06 09:45:50 +0800

    Initial Commit

---
+++ AVtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVtest
+//
+//  Created by 周全营 on 2019/10/6.
+//  Copyright © 2019 周全营. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
